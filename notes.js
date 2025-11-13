7444a845fc4f3b58925f4d2a1c61ba49542f924c
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-11-12 16:40:52 -1100

    Initial

---
+++ notes.js
@@ -0,0 +1,16 @@
+//  https://www.youtube.com/watch?v=PTlCq7GEpCo
+//  from Lesson 3: classes & OOP - Abstract Classes
+//  as per lesson 3 https://ps-lms.vercel.app/curriculum/se/413/lab-1#:~:text=Create%20the%20Product%20Base%20Class%3A
+// https://www.typescriptlang.org/docs/handbook/2/classes.html#abstract-
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+// https://javascript.info/
+// https://www.typescriptlang.org/docs/handbook/modules/introduction.html
+// https://www.typescriptlang.org/docs/handbook/modules/introduction.html#export
+// https://www.typescriptlang.org/docs/handbook/modules.html#exporting
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+// https://www.typescripttutorial.net//typescript-tutorial/typescript-date/
+// https://www.w3schools.com/jsref/jsref_obj_date.asp
+// https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#arrays
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+// https://www.geeksforgeeks.org/typescript/method-overriding-in-typescript/
